7fe5e4c2bdb4080c1c7e676610ce0c7a621a6f54
Nils Ramsperger <nils@ramsperger-home.de>
2020-03-29 15:07:14 +0200

    Removed unused install commands

--- files/supervisor.sh
+++ files/supervisor.sh
@@ -25,11 +25,6 @@ term_handler() {
 install() {
     echo "Installing Insurgency Sandstorm Dedicated Server"
     /opt/steam/steamcmd.sh +login anonymous +force_install_dir /opt/steam/insurgency +app_update 581330 validate +quit
-    #cp -a /tmp/mapcycle_cooperative.txt /opt/steam/insurgency/insurgency/
-    #cp -a /tmp/cfg/. /opt/steam/insurgency/insurgency/cfg/
-    #mkdir -p /opt/steam/insurgency/insurgency/scripts
-    #cp -a /tmp/scripts/. /opt/steam/insurgency/insurgency/scripts/
-    #rm -r /tmp/
     chown -R steam:steam /opt/steam/insurgency
     echo "Installation done"
 }
